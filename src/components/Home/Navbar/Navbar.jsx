dd95945ea62aea0757c9ea0d2f793d35d75735f6
Vanshika Aggarwal <vanshikaagarwal8484@gmail.com>
2025-11-11 17:39:58 +0530

    Update Navbar.jsx

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -44,12 +44,12 @@ export default function LusionNavbar() {
 
       {/* LEFT SIDE VERTICAL LOGO */}
       <div className="fixed left-16 top-12 z-50">
-        <p
+        <h3
           className="text-white text-3xl md:text-4xl lg:text-5xl font-bold italic leading-none tracking-tight"
           style={{ fontFamily: "'Montserrat', sans-serif" }}
         >
           HEYFYNIX
-        </p>
+        </h3>
       </div>
 
       {/* RIGHT SIDE BUTTON GROUP */}
