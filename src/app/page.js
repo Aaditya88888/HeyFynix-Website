ea1c53140d373093381d1b3894ad969ad21aeb7f
kamil <kamilmalik.mk.001@gmail.com>
2025-11-20 19:40:02 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -23,8 +23,8 @@ export default function Page() {
       <section className="min-h-screen flex items-center justify-center bg-black">
         <CreativeText
           startOffset={1.3} 
-          revealRange={0.2} 
-          holdDuration={0.13} 
+          revealRange={0.15} 
+          holdDuration={0.7} 
         />
       </section>
 
