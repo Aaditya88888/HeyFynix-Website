5e9582928dc84d7887ad20a35052822ef25dc8d2
kamil <kamilmalik.mk.001@gmail.com>
2025-12-16 13:35:13 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import CursorEffect from "@/components/CursorEffect/CursorEffect";
 import StarsBackground from "@/components/StarsBackground/StarsBackground";
 import ClientParallaxProvider from "./ParallaxProvider";
-import LoadingOverlay from "@/components/LoadingOverlay";
 const inter = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
@@ -26,9 +25,7 @@ export default function RootLayout({ children }) {
         <StarsBackground />
         <CursorEffect />
 
-        {/* Loading Overlay */}
-        <LoadingOverlay />
-
+      
         {/* Main Content */}
         <ClientParallaxProvider>{children}</ClientParallaxProvider>
       </body>
