42c7cd62b964835bda02962b70e51eb51119548f
vanshika8484 <vanshikaagarwal8484@gmail.com>
2025-12-18 19:04:50 +0530

    add some changes in earth section

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['your-image-domain.com'],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname, useRouter } from "next/navigation";
-import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function LusionNavbar() {
   const [soundOn, setSoundOn] = useState(false);
